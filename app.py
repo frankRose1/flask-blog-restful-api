8a69fff023aeb38ce3610b0b7625425a14d37abf
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-01-11 17:17:17 -0500

    Revert "Initial commit and add some dependencies"
    This reverts commit 2279bf49d97696fedf1b3727547de92e8f0aefdc.

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from app_config import config
-
-app = Flask(__name__)
-
-if __name__ =='__main__':
-    app.run(debug=config['DEBUG'], host=config['HOST'], port=config['HOST'])
